846c32a281d647955181f166c9958248b36170d3
yadhukrishnakb <yadhubabu151@gmail.com>
2026-09-03 18:15:42 +0530

    added product sorting based on time

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,23 +1,25 @@
-const Product = require("../models/Product")
+const Product = require("../models/Product");
 
 const getProducts = async (request, response) => {
-    try {
-        const products = await Product.find()
-        if (products.length === 0) {
-            return response.status(404).json({ message: "No Products Available!" })
-        }
-
-        let categories = []
-        products.forEach(each => {
-            if (!categories.includes(each.category)){
-                categories.push(each.category)
-            }
-        })
-
-        return response.status(200).json({ products, categories })
-    } catch (err) {
-        return response.status(500).json({ message: `Server Error: ${err.message}` })
+  try {
+    const products = await Product.find().sort({ createdAt: -1 });
+    if (products.length === 0) {
+      return response.status(404).json({ message: "No Products Available!" });
     }
-}
 
-module.exports = { getProducts }
+    let categories = [];
+    products.forEach((each) => {
+      if (!categories.includes(each.category)) {
+        categories.push(each.category);
+      }
+    });
+
+    return response.status(200).json({ products, categories });
+  } catch (err) {
+    return response
+      .status(500)
+      .json({ message: `Server Error: ${err.message}` });
+  }
+};
+
+module.exports = { getProducts };
--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    title: {type: String,required: true},
-    image_url: {type: String,required: true},
-    description: {type: String,required: true},
-    category: {type: String,required: true,uppercase: true},
-    product_link: {type: String,required: true}
-})
+  title: { type: String, required: true },
+  image_url: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true, uppercase: true },
+  product_link: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
-module.exports = mongoose.model("Product",productSchema)
+module.exports = mongoose.model("Product", productSchema);
